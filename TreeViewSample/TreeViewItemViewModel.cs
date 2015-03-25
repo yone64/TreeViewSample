54cec3dc279a06544f56923b477de59745556631
daisuke.yoneda <daisuke.yoneda@basic.co.jp>
2015-03-25 23:55:25 +0900

    VM追加

---
+++ TreeViewSample/TreeViewItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TreeViewSample
+{
+    public class TreeViewItemViewModel
+    {
+        public string Header { get; set; }
+
+        public List<TreeViewItemViewModel> Children { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
